c4272707980bb907a8bacc686612af40672866ca
8BitsOneByte <201945071+8BitsOneByte@users.noreply.github.com>
2026-03-23 17:24:53 +0800

    WebProfileViewer1.4.6 Solved the typo in mod name Solved the jar name is not the same as mod version

--- src/client/java/org/exmple/webprofileviewer/client/GlobalConstants.java
+++ src/client/java/org/exmple/webprofileviewer/client/GlobalConstants.java
@@ -1,7 +1,16 @@
 package org.exmple.webprofileviewer.client;
 
+import net.fabricmc.loader.api.FabricLoader;
+
 public class GlobalConstants {
     public static final String NOT_FOUND = "未找到";
     public static final String NAMESPACE = "webprofileviewer";
-    public static final String VERSION = "1.4.6";
+    public static final String VERSION = resolveVersion();
+
+    private static String resolveVersion() {
+        return FabricLoader.getInstance()
+            .getModContainer(NAMESPACE)
+            .map(container -> container.getMetadata().getVersion().getFriendlyString())
+            .orElse("unknown");
+    }
 }
